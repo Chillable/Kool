45eb41101a4328a7405216167630160cadf44076
Chillable <101722406+Chillable@users.noreply.github.com>
2022-03-28 08:01:44 +0800

    Removed Storage Library
    The Storage Library has been made into another module.

--- Kool.lua
+++ Kool.lua
@@ -5,7 +5,7 @@ Kool, a cool game framework
 Author: xFly_Flame1014
 Date: 23/3/2022
 
-Version 1.1
+Version 1.5
 
 Github: https://github.com/Chillable/Kool
 
@@ -17,6 +17,8 @@ local RunService = game:GetService("RunService")
 --//Containers
 local Services = {}
 local Controllers = {}
+
+local Modules = {}
 
 --//Modules
 local KoolStorage = require(script.Modules.KoolStorage)
@@ -31,6 +33,7 @@ local clientStarted = false
 export type Service = {
 
 	Name: string,
+	Client: {[any]: any},
 	[any]: any
 
 }
@@ -51,12 +54,27 @@ export type Storage = {
 
 }
 
+export type Module = {
+
+	Name: string,
+	[any]: any
+
+}
+
 --//Main
 
 --//Server
 local KoolServer do 
 
 	KoolServer = {}
+
+	local folder = Instance.new("Folder")
+
+	folder.Name = "Remotes"
+
+	KoolServer.RunService = game:GetService("RunService")
+
+	KoolServer.RemotesFolder = folder
 
 	function KoolServer:Start()
 
@@ -115,8 +133,6 @@ local KoolServer do
 
 				require(module)
 
-				self:AddServices(module)
-
 			end
 
 		end
@@ -128,6 +144,12 @@ local KoolServer do
 		assert(typeof(serviceData) == "table", OutputFormat:format("Argument #1 must be a table"))
 		assert(typeof(serviceData.Name) == "string", OutputFormat:format("Service's Name must be a string"))
 		assert(Services[serviceData.Name] == nil, OutputFormat:format(serviceData.Name.." already existed"))
+
+		if Services[serviceData.Name] then
+
+			return OutputFormat:format(serviceData.Name.." already exists")
+
+		end
 
 		Services[serviceData.Name] = serviceData
 
@@ -182,17 +204,107 @@ local KoolServer do
 		end
 
 	end
-	
-	KoolServer.CreateStorage = KoolStorage.CreateStorage
-	KoolServer.GetStorage = KoolStorage.GetStorage
-	KoolServer.RemoveStorage = KoolStorage.RemoveStorage
-	
+
+	function KoolServer:GetAllServices()
+
+		if serverStarted then
+
+			warn(OutputFormat:format("All Services aren't available until Kool has started"))
+
+		else
+
+			local services = {}
+
+			for i, service in pairs(Services) do
+
+				table.insert(services, service.Name)
+
+			end
+
+			return services
+
+		end
+
+	end
+
+	function KoolServer:CreateModule(module: Module)
+
+		assert(typeof(module) == "table" , OutputFormat:format("Argument #1 must be a table"))
+		assert(typeof(module.Name), OutputFormat:format("Module's Name must be a string"))
+
+		if Modules[module.Name] then
+
+			return warn(OutputFormat:format(module.Name.." already exists"))
+
+		else 
+
+			Modules[module.Name] = module
+
+			return module
+
+		end
+
+	end
+
+	function KoolServer:GetModule(name: string)
+
+		assert(typeof(name) == "string", OutputFormat:format("Argument #1 must be a string"))
+
+		if Modules[name] then
+
+			return Modules[name]
+
+		else
+
+			warn(OutputFormat:format(name.." does not exist"))
+
+		end
+
+	end
+
+	function KoolServer:RemoveModule(name: string)
+
+		assert(typeof(name) == "string", OutputFormat:format("Argument #1 must be a string"))
+
+		if Modules[name] then
+
+			Modules[name] = nil
+
+		else
+
+			warn(OutputFormat:format(name.." does not exist"))
+
+		end
+
+	end
+
+	function KoolServer:AddModules(directory: Instance)
+
+		assert(typeof(directory) == "Instance", OutputFormat:format("Argument #1 must be an Instance"))
+
+		for i, module in pairs(directory:GetChildren()) do
+
+			if module:IsA("ModuleScript") then
+
+				require(module)
+
+				self:AddModules(module)
+
+			end
+
+		end
+
+	end
+
 end
 
 --//Client
 local KoolClient do
 
 	KoolClient = {}
+
+	KoolClient.Player = game:GetService("Players").LocalPlayer
+	KoolClient.RunService = game:GetService("RunService")
 
 	function KoolClient:Start()
 
@@ -263,6 +375,12 @@ local KoolClient do
 		assert(typeof(controllerData.Name) == "string", OutputFormat:format("Controller's Name must be a string"))
 		assert(Services[controllerData.Name] == false, OutputFormat:format(controllerData.Name.." already existed"))
 
+		if Controllers[controllerData.Name] then
+
+			return warn(OutputFormat:format(controllerData.Name.." already exists"))
+
+		end
+
 		Controllers[controllerData.Name] = controllerData
 
 		RunService.Heartbeat:Wait()
@@ -279,7 +397,7 @@ local KoolClient do
 
 		RunService.Heartbeat:Wait()
 
-		if serverStarted then
+		if clientStarted then
 
 			if controller then 
 
@@ -297,6 +415,28 @@ local KoolClient do
 
 		end
 
+	end
+	
+	function KoolClient:GetAllControllers()
+		
+		if clientStarted then
+
+			warn(OutputFormat:format("All Controllers aren't available until Kool has started"))
+
+		else
+
+			local controllers = {}
+
+			for i, controller in pairs(Controllers) do
+
+				table.insert(controllers, controller.Name)
+
+			end
+
+			return controllers
+
+		end
+		
 	end
 
 	function KoolClient:RemoveController(name:  string)
@@ -316,9 +456,57 @@ local KoolClient do
 		end
 
 	end
-	
-	KoolClient.GetStorage = KoolStorage.GetStorage
-	
+
+	function KoolClient:GetModule(name: string)
+
+		assert(typeof(name) == "string", OutputFormat:format("Argument #1 must be a string"))
+
+		if Modules[name] then
+
+			return Modules[name]
+
+		else
+
+			warn(OutputFormat:format(name.." does not exist"))
+
+		end
+
+	end
+
+	function KoolClient:GetService(name: string)
+
+		assert(typeof(name) == "string", OutputFormat:format("Argument #1 must be a string"))
+
+		local service = Services[name]
+
+		if serverStarted then
+
+			if service then
+
+				if service.Client then
+
+					return service.Client
+
+				else
+
+					warn(OutputFormat:format(name.." is not available to Client"))
+
+				end
+
+			else
+
+				warn(OutputFormat:format(name.." does not exist"))
+
+			end
+
+		else
+
+			warn(OutputFormat:format("All Services aren't available until Kool has started"))
+
+		end
+
+	end
+
 end
 
 if RunService:IsServer() then
@@ -332,4 +520,3 @@ if RunService:IsClient() then
 	return KoolClient
 
 end
-
